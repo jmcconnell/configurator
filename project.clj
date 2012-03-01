baf08903193ee61a6833cd6d1bc64717a24dcd45
J. McConnell <j@ubermenschconsulting.com>
2012-03-01 14:55:03 -0500

    Fixed project name and set version to 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.ubermenschconsulting.configurator "1.0.0-SNAPSHOT"
+(defproject com.ubermenschconsulting/configurator "0.1.0-SNAPSHOT"
   :description "Allows for the registration and monitoring of config files."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [joda-time/joda-time "1.6"]])
