9daf0f3e9cefb83971af02bff43d07d3302bb3ec
J. McConnell <j@ubermenschconsulting.com>
2012-03-02 23:27:26 -0500

    Bumped version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.ubermenschconsulting/configurator "0.1.1"
+(defproject com.ubermenschconsulting/configurator "0.2.0-SNAPSHOT"
   :description "Allows for the registration and monitoring of config files."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [joda-time/joda-time "1.6"]]
